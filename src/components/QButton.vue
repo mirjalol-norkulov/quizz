df81af68543df2052fc58260bda21687ff327f35
Mirjalol Norqulov <mirjalol2401@gmail.com>
2021-10-09 20:08:07 +0500

    added new components

---
+++ src/components/QButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <component :is="component">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: "QButton",
+  props: {
+    href: {
+      type: String,
+    },
+    to: {
+      type: [Object, String],
+    },
+  },
+  computed: {
+    component() {
+      if (this.href) {
+        return "a";
+      }
+      if (this.to) {
+        return "router-link";
+      }
+
+      return "button";
+    },
+  },
+};
+</script>
